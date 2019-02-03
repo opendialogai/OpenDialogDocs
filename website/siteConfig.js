c656a22548606461904a0ff066fcdbc73d4a8cb3
Ronald Ashri <ronald@istos.it>
2019-02-03 17:18:32 +0100

    config update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'OpenDialog', // Title for your website.
   tagline: 'Conversation Management Platform',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://greenshootlabs.github.io', // Your website URL
+  baseUrl: '/opendialogdocs', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'opendialogdocs',
+  organizationName: 'greenshootlabs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
